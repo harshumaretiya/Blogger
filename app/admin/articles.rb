e1478f463efc52030d2a6d53b9b2a3305145ff00
harshumaretiya <harsh.patel.hp846@gmail.com>
2021-04-21 22:49:18 +0530

    Feat: Active admin gem added for admin panel

---
+++ app/admin/articles.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Article do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :body, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :view_count, :author_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :body, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :view_count, :author_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/authors.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Author do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :username, :email, :crypted_password, :salt
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:username, :email, :crypted_password, :salt]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
